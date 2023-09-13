4abe8c1c25605b26b043272ff81198871cfa0ae6
чилл <70902048+bllooop@users.noreply.github.com>
2023-09-14 01:57:20 +0300

    Update NewsApiInterface.kt

--- app/src/main/java/com/example/musicandfilm/services/news/NewsApiInterface.kt
+++ app/src/main/java/com/example/musicandfilm/services/news/NewsApiInterface.kt
@@ -9,10 +9,10 @@ import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface NewsApiInterface {
-    @GET("wall.get?owner_id=-108468&domain=kinopoisk&count=20&extended=1&v=5.131&access_token=2300f98b2300f98b2300f98bc12013d013223002300f98b4718217f8b8176811f7b0136")
+    @GET("wall.get?owner_id=-108468&domain=kinopoisk&count=20&extended=1&v=5.131&access_token=")
     fun getNewsList(): Call<NewsResponse>
 
-    @GET ("wall.getById?extended=1&v=5.131&access_token=2300f98b2300f98b2300f98bc12013d013223002300f98b4718217f8b8176811f7b0136")
+    @GET ("wall.getById?extended=1&v=5.131&access_token=")
     fun getNewsDetail(@Query("posts") id: String): Call<NewsResponse>
     @GET("wall.get?owner_id=-12256840&count=20&extended=1&v=5.131&access_token=2300f98b2300f98b2300f98bc12013d013223002300f98b4718217f8b8176811f7b0136")
     fun getNewsListMusic(): Call<NewsResponse>
